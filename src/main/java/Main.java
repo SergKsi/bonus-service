aca17b034cf4ad2eb0ad1d989479fd41b613d30f
Sergey Kosovichev <sergksi@mail.ru>
2022-08-16 18:37:06 +0300

    fixed #1 Add output Bonus

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
-
-    service.calculate(amount, registered);
-  }
+        long amount = 1000_60;
+        boolean registered = true;
+        // до изменения  - метод вычисляется,но не возвращает значение Бонуса
+        // и Бонус далее в программе нигде не выводится и не используется
+        // service.calculate(amount, registered);
+        // присваиваем значения Бонуса в переменную bonus и выводим в консоль
+        long bonus = service.calculate(amount, registered);
+        System.out.println("bonus = " + bonus);
+    }
 }
 
